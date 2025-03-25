db862fc96c1c0a54a708e533edcf30b7ed45d413
tuanvuong102 <129262843+tuanvuong102@users.noreply.github.com>
2025-03-25 17:23:23 +0700

    Add files via upload

---
+++ lession01-spring-boot/lession01-spring-boot/src/test/java/com/devmaster/lession01_spring_boot/Lession01SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmaster.lession01_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lession01SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
