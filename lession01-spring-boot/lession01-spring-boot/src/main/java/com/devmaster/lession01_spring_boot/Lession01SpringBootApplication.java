2417604df6342a24e14ca06d04da6e2ff03d88cb
tuanvuong102 <129262843+tuanvuong102@users.noreply.github.com>
2025-03-31 14:10:51 +0700

    Add files via upload

---
+++ lession01-spring-boot/lession01-spring-boot/src/main/java/com/devmaster/lession01_spring_boot/Lession01SpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.devmaster.lession01_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lession01SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lession01SpringBootApplication.class, args);
+		System.out.println("Hello World!");
+
+	}
+}
